2794a92fda5bba4d9ec01779d5ae919354e51e1c
CamilleAbella <camille.abella@hotmail.fr>
2020-04-16 16:30:34 +0200

    update readme and prepare TODO for dependencies installation

--- boot.js
+++ boot.js
@@ -30,6 +30,7 @@ async function installDependencies(){
         '' : 'npm init -y &&';
     return new Promise((resolve,reject) => {
         cp.exec(`cd .. && ${include} npm install`, e => {
+            // TODO: link the stdout with console to detail installation.
             if(e) reject(e);
             resolve();
         })
@@ -38,6 +39,7 @@ async function installDependencies(){
 
 (async ()=>{
     let code = 0
+    console.log('File copying...');
     try{
         await copyDir('./project_files/','../');
         console.log('File copying','FINISH');
@@ -45,6 +47,7 @@ async function installDependencies(){
         console.log('File copying','ERROR',e.message);
         code = 1
     }
+    console.log('Dependencies installation...');
     try{
         await installDependencies()
         console.log('Dependencies installation','FINISH');
