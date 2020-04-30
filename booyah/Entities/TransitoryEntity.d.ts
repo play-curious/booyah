6f20e96c0d0f40fd8bb056bf022d34fc9018373a
CamilleAbella <camille.abella@hotmail.fr>
2020-04-30 12:25:03 +0200

    reset structure

--- booyah/Entities/TransitoryEntity.d.ts
+++
@@ -1,7 +0,0 @@
-import Entity from './Entity';
-/** An entity that returns the requested transition immediately  */
-export default class TransitoryEntity extends Entity {
-    transition: boolean;
-    constructor(transition?: boolean);
-    _setup(): void;
-}
--- src/Entities/NullEntity.ts
+++
@@ -1,4 +0,0 @@
-import Entity, {EntityConfig, TearDownOptions, UpdateOptions} from "./Entity";
-
-/** Empty class just to indicate an entity that does nothing and never requests a transition  */
-export default class NullEntity extends Entity {}
