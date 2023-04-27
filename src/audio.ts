a7b0e1de5a1a75492d24b5f5c587371e514cea09
Jesse Himmelstein <jesse@heavenstone.net>
2023-04-27 17:33:07 +0200

    Added audio

--- src/audio.ts
+++ src/audio.ts
@@ -1,12 +1,9 @@
-import * as util from "./util";
-import * as chip from "./chip";
-import { Howl, Howler } from "howler";
-import _ from "underscore";
+import { sound } from "@pixi/sound";
 
-export const AUDIO_FILE_FORMATS = ["mp3"];
+import * as chip from "booyah/src/chip";
 
-export interface JukeboxOptions {
-  volume?: number;
+export class JukeboxOptions {
+  volume = 0.5;
 }
 
 /** 
@@ -14,109 +11,72 @@ export interface JukeboxOptions {
   By default the volume is lowered to not interfere with sound effects.
 */
 export class Jukebox extends chip.ChipBase {
-  public volume: number;
-  public musicName: string;
-  public musicPlaying: Howl;
-  public muted: boolean;
+  private _options: JukeboxOptions;
+  private _volume: number;
+  private _musicName?: string;
 
-  constructor(options: JukeboxOptions = {}) {
+  constructor(options?: Partial<JukeboxOptions>) {
     super();
 
-    util.setupOptions(this, options, {
-      volume: 0.25,
-    });
+    this._options = chip.fillInOptions(options, new JukeboxOptions());
   }
 
-  _onActivate() {
-    this.musicName = null;
-    this.musicPlaying = null;
-
-    _.each(this._chipContext.musicAudio, (howl: Howl) => {
-      howl.loop(true);
-    });
-
-    this.muted = this._chipContext.muted;
-    this._updateMuted();
-
-    this._on(this._chipContext.playOptions, "musicOn", this._updateMuted);
+  protected _onActivate(): void {
+    this._volume = this._options.volume;
   }
 
   _onTerminate() {
-    if (this.musicPlaying) this.musicPlaying.stop();
-    this.musicPlaying = null;
-    this.musicName = null;
+    if (this._musicName) sound.stop(this._musicName);
+
+    delete this._musicName;
   }
 
-  _onSignal(tickInfo: chip.TickInfo, signal: string, data?: any) {
-    if (!this.musicPlaying) return;
-
-    if (signal === "pause") this.musicPlaying.pause();
-    else if (signal === "play") this.musicPlaying.play();
-    else if (signal === "reset") this.play();
+  protected _onPause(): void {
+    if (this._musicName) sound.pause(this._musicName);
   }
 
-  changeVolume(volume: number) {
-    this.volume = volume;
-    if (this.musicPlaying) this.musicPlaying.volume(volume);
+  protected _onResume(): void {
+    if (this._musicName) sound.resume(this._musicName);
   }
 
-  play(name?: string, volume?: number) {
-    if (this.musicPlaying && this.musicName === name) return;
+  pauseMusic(): void {
+    if (this._musicName) sound.pause(this._musicName);
+  }
 
-    if (this.musicPlaying) {
-      // TODO: fade
-      this.musicPlaying.stop();
-      this.musicPlaying = null;
-    }
+  resumeMusic(): void {
+    if (this._musicName) sound.resume(this._musicName);
+  }
 
-    // If no new music is requested, stop
-    if (!name) return;
+  get volume(): number {
+    return this._volume;
+  }
 
-    if (!(name in this._chipContext.musicAudio)) {
-      console.error("Missing music", name);
-      return;
-    }
+  set volume(value: number) {
+    this._volume = value;
+    if (this._musicName) sound.volume(this._musicName, value);
+  }
 
-    if (name) {
-      this.musicName = name;
-      this.musicPlaying = this._chipContext.musicAudio[name];
-      this.musicPlaying.volume(volume ?? this.volume);
-      this.musicPlaying.play();
-    }
+  play(name: string) {
+    if (this._musicName === name) return;
+
+    this.stop();
+
+    sound.play(name, {
+      volume: this._volume,
+      loop: true,
+    });
   }
 
   stop() {
-    this.play();
-  }
+    if (!this._musicName) return;
 
-  setMuted(isMuted: boolean) {
-    this.muted = isMuted;
-    this._updateMuted();
-  }
-
-  _updateMuted() {
-    const muted = !this._chipContext.playOptions.options.musicOn;
-    _.each(this._chipContext.musicAudio, (howl: Howl) => howl.mute(muted));
+    sound.stop(this._musicName);
+    delete this._musicName;
   }
 }
 
-export function installJukebox(
-  rootConfig: chip.ChipContext,
-  rootChip: chip.Parallel
-) {
-  rootConfig.jukebox = new Jukebox();
-  rootChip.addChildChip(rootConfig.jukebox);
-}
-
-export function makeInstallJukebox(options: JukeboxOptions) {
-  return (rootConfig: chip.ChipContext, rootChip: chip.Parallel) => {
-    rootConfig.jukebox = new Jukebox(options);
-    rootChip.addChildChip(rootConfig.jukebox);
-  };
-}
-
-/** 
-  Am chip that requests the music be changed upon activate.
+/**
+  A chip that requests the music be changed upon activate.
   Optionally can stop the music on terminate.
 */
 export class MusicChip extends chip.ChipBase {
@@ -124,15 +84,14 @@ export class MusicChip extends chip.ChipBase {
     super();
   }
 
-  _onActivate(tickInfo: chip.TickInfo, chipContext: chip.ChipContext) {
+  _onActivate() {
     this._chipContext.jukebox.play(this.trackName);
-
-    this._outputSignal = chip.makeSignal();
+    this.terminate();
   }
 
   _onTerminate() {
     if (this.stopOnTeardown) {
-      this._chipContext.jukebox.play();
+      this._chipContext.jukebox.stop();
     }
   }
 }
@@ -141,78 +100,61 @@ export class FxMachineOptions {
   volume = 1;
 }
 
-/** 
+class PlayingSoundOptions {
+  volumeScale = 1;
+  loop = false;
+}
+
+/**
   Play sounds effects.
 */
 export class FxMachine extends chip.ChipBase {
+  private _options: FxMachineOptions;
   private _volume: number;
-
-  private _playingSounds: Record<string, { volumeScale: number }>;
+  private _playingSounds: Record<string, PlayingSoundOptions>;
 
   constructor(options?: Partial<FxMachineOptions>) {
     super();
 
-    const _options = util.fillInOptions(options, new FxMachineOptions());
-    this._volume = _options.volume;
+    this._options = chip.fillInOptions(options, new FxMachineOptions());
   }
 
   _onActivate() {
-    this.changeVolume(this._volume);
-
-    this._updateMuted();
-
-    this._on(this._chipContext.playOptions, "fxOn", this._updateMuted);
-
+    this._volume = this._options.volume;
     this._playingSounds = {};
-    for (const name in this._chipContext.fxAudio) {
-      const howl = this._chipContext.fxAudio[name];
-
-      // The Howl.on() function doesn't take the same arguments as the event emitter, so we don't use this._on()
-      howl.on("end", () => {
-        // console.log("fx ended", name);
-
-        // A sound can be looping, so don't remove it from our list until it is really done
-        if (howl.playing()) return;
-
-        delete this._playingSounds[name];
-      });
-    }
   }
 
-  protected _onTerminate(tickInfo: chip.TickInfo): void {
+  protected _onTerminate(): void {
     this.stopAll();
-  }
-
-  changeVolume(volume: number) {
-    this._volume = volume;
-    for (const name in this._playingSounds) {
-      const howl = this._chipContext.fxAudio[name];
-      howl.volume(volume * this._playingSounds[name].volumeScale);
-    }
   }
 
   /** Returns sound duration in ms */
   getDuration(name: string): number {
-    return this._chipContext.fxAudio[name].duration() * 1000;
+    return sound.duration(name) * 1000;
   }
 
-  play(name: string, options = { volumeScale: 1, loop: false }) {
-    if (!(name in this._chipContext.fxAudio)) {
-      console.error("Missing sound effect", name);
-      return;
-    }
+  play(name: string, options?: Partial<PlayingSoundOptions>) {
+    if (!sound.exists(name)) throw new Error(`Missing sound effect ${name}`);
 
-    // console.log("fx playing", name);
+    const completeOptions = chip.fillInOptions(
+      options,
+      new PlayingSoundOptions()
+    );
 
-    const howl = this._chipContext.fxAudio[name];
-    howl.volume(this._volume * options.volumeScale);
-    howl.loop(options.loop);
-    howl.play();
-    this._playingSounds[name] = { volumeScale: options.volumeScale };
+    sound.play(name, {
+      volume: this._volume * completeOptions.volumeScale,
+      loop: completeOptions.loop,
+      complete: () => {
+        delete this._playingSounds[name];
+        this.emit("complete", name);
+      },
+    });
+
+    this._playingSounds[name] = completeOptions;
   }
 
   stop(name: string): void {
-    this._chipContext.fxAudio[name].stop();
+    sound.stop(name);
     delete this._playingSounds[name];
   }
 
@@ -222,81 +164,86 @@ export class FxMachine extends chip.ChipBase {
     }
   }
 
-  pause(name: string): void {
-    this._chipContext.fxAudio[name].pause();
+  pauseSound(name: string): void {
+    sound.pause(name);
   }
 
   pauseAll(): void {
     // console.log("Pausing all sounds", this._playingSounds);
     for (const name in this._playingSounds) {
-      this.pause(name);
+      this.pauseSound(name);
     }
   }
 
-  resume(name: string): void {
-    this._chipContext.fxAudio[name].play();
+  resumeSound(name: string): void {
+    sound.resume(name);
   }
 
   resumeAll(): void {
     for (const name in this._playingSounds) {
-      this.resume(name);
+      this.resumeSound(name);
     }
   }
 
-  // TODO: stop playing effects when paused
-  protected _onSignal(
-    tickInfo: chip.TickInfo,
-    signal: string,
-    data?: any
-  ): void {
-    if (signal === "pause") this.pauseAll();
-    if (signal === "play") this.resumeAll();
+  protected _onPause(): void {
+    sound.pauseAll();
   }
 
-  _updateMuted() {
-    const muted = !this._chipContext.playOptions.options.fxOn;
-    _.each(this._chipContext.fxAudio, (howl: Howl) => howl.mute(muted));
+  protected _onResume(): void {
+    sound.resumeAll();
   }
-}
 
-export function installFxMachine(rootConfig: any, rootChip: any) {
-  rootConfig.fxMachine = new FxMachine();
-  rootChip.addChildChip(rootConfig.fxMachine);
-}
+  get volume(): number {
+    return this._volume;
+  }
 
-export function makeInstallFxMachine(options: FxMachineOptions) {
-  return (rootConfig: chip.ChipContext, rootChip: chip.Parallel) => {
-    rootConfig.fxMachine = new FxMachine(options);
-    rootChip.addChildChip(rootConfig.fxMachine);
-  };
-}
+  set volume(value: number) {
+    this._volume = value;
 
-/** Creates a Promise from the Howl callbacks used for loading */
-
-export function makeHowlerLoadPromise(howl: Howl) {
-  return new Promise((resolve, reject) => {
-    howl.on("load", () => resolve(howl));
-    howl.on("loaderror", (id, err) => reject({ howl, id, err }));
-  });
-}
-
-/** Create map of file names or {key, url} to Howl objects */
-export function makeHowls(
-  directory: string,
-  assetDescriptions: (string | { key: string; url: string })[]
-) {
-  const assets: { [key: string]: Howl } = {};
-  for (const assetDescription of assetDescriptions) {
-    if (_.isString(assetDescription)) {
-      assets[assetDescription] = new Howl({
-        src: assetDescription,
-      });
-    } else {
-      const url = assetDescription.url;
-      assets[assetDescription.key] = new Howl({
-        src: assetDescription.url,
-      });
+    for (const name in this._playingSounds) {
+      sound.volume(name, value);
     }
   }
-  return assets;
 }
+
+// export function installFxMachine(rootConfig: any, rootChip: any) {
+//   rootConfig.fxMachine = new FxMachine();
+//   rootChip.addChildChip(rootConfig.fxMachine);
+// }
+
+// export function makeInstallFxMachine(options: FxMachineOptions) {
+//   return (rootConfig: chip.ChipContext, rootChip: chip.Parallel) => {
+//     rootConfig.fxMachine = new FxMachine(options);
+//     rootChip.addChildChip(rootConfig.fxMachine);
+//   };
+// }
+
+// /** Creates a Promise from the Howl callbacks used for loading */
+
+// export function makeHowlerLoadPromise(howl: Howl) {
+//   return new Promise((resolve, reject) => {
+//     howl.on("load", () => resolve(howl));
+//     howl.on("loaderror", (id, err) => reject({ howl, id, err }));
+//   });
+// }
+
+// /** Create map of file names or {key, url} to Howl objects */
+// export function makeHowls(
+//   directory: string,
+//   assetDescriptions: (string | { key: string; url: string })[]
+// ) {
+//   const assets: { [key: string]: Howl } = {};
+//   for (const assetDescription of assetDescriptions) {
+//     if (_.isString(assetDescription)) {
+//       assets[assetDescription] = new Howl({
+//         src: assetDescription,
+//       });
+//     } else {
+//       const url = assetDescription.url;
+//       assets[assetDescription.key] = new Howl({
+//         src: assetDescription.url,
+//       });
+//     }
+//   }
+//   return assets;
+// }
